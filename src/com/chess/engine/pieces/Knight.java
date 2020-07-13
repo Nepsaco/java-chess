83af0341c11efb5e4bc3d68c96cb1196d0524fd0
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2020-07-13 14:42:40 -0500

    knight piece funtionallity

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -4,12 +4,14 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Knight extends Piece {
 
   private final static int[] CANDIDATE_MOVE_COORDINATES = { -17, -15, -10, -6, 6, 10, 15, 17 };
 
-  Knight(final int piecePostion, final Alliance pieceAlliance) {
+  Knight(final int piecePosition, final Alliance pieceAlliance) {
     super(piecePosition, pieceAlliance);
   }
 
@@ -17,17 +19,29 @@ public class Knight extends Piece {
   public List<Move> calculateLegalMoves(Board board) {
 
     int candidateDestinationCoordinate;
+    final List<Move> legalMoves = new ArrayList<>();
 
     for(final int currentCandidate : CANDIDATE_MOVE_COORDINATES) {
-      candidateDestinationCoordinate = this.piecePostion + currentCandidate;
-        if(true){
-          final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
-        }
-      if(candidateDestinaionTile.isTileOccupied()) {
 
-      }
+      candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+
+        if(true){
+
+          final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+          if(candidateDestinaionTile.isTileOccupied()) {
+            legalMoves.add(new Move());
+          } else {
+            final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+            final Alliance pieceAlliance = pieceAtDestination;
+
+            if(this.pieceAlliance != pieceAlliance) {
+              legalMoves.add(new Move());
+            }
+          }
+        }
     }
 
-    return null
+    return Collections.unmodifiableMap(legalMoves);
   }
 }
